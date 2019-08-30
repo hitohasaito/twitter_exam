84355d4102e6a3f937eebabcc81ee8fcef6d057a
齋藤一葉 <saitouhitoha@saitouichiyounoMacBook-puro.local>
2019-08-30 20:01:10 +0900

    【Add】新規投稿機能を作成

--- app/controllers/twitters_controller.rb
+++ app/controllers/twitters_controller.rb
@@ -1,2 +1,20 @@
 class TwittersController < ApplicationController
+  def new
+    @twitter = Twitter.new
+  end
+  def create
+    @twitters = Twitter.new(twi_params)
+    if @twitters.save
+      redirect_to twitters_path
+    else
+      render "new"
+    end
+  end
+
+
+private
+
+ def twi_params
+   params.require(:twitter).permit(:content)
+ end
 end
