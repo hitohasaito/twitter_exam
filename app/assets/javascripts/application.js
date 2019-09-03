12c003b37d5ef50cd07cd2266158f9f0e9f7a880
齋藤一葉 <saitouhitoha@saitouichiyounoMacBook-puro.local>
2019-09-03 20:51:43 +0900

    init

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery/dist/jquery.min.js
+//= require jquery/dist/jquery.js
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
